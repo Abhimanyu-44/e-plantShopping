f92674a2c58217022dbe93d59c793d3346bade4e
Abhimanyu-44 <abhimanyudas877@gmail.com>
2025-12-14 12:51:32 +0530

    productlist

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -11,6 +11,29 @@ function ProductList({ onHomeClick }) {
 
   const dispatch = useDispatch();
   const cart = useSelector(state => state.cart.items);
+  const styleObj = {
+  backgroundColor: '#4CAF50',
+  color: '#fff',
+  padding: '15px',
+  display: 'flex',
+  justifyContent: 'space-between',
+  alignItems: 'center', // make sure this spelling is correct
+  fontSize: '20px',
+};
+
+const styleObjUl = {
+  display: 'flex',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+  width: '1100px',
+};
+
+const styleA = {
+  color: 'white',
+  fontSize: '30px',
+  textDecoration: 'none',
+};
+
 
   const plantsArray = [
     {
