41816f5a370d9d5e2140219dc8ef2ad5f62e28c6
Abhimanyu-44 <abhimanyudas877@gmail.com>
2025-12-14 11:00:57 +0530

    cartitems

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { removeItem, updateQuantity } from "../redux/CartSlice";
+import { removeItem, updateQuantity } from "./CartSlice";
+
 import { useNavigate } from "react-router-dom";
 
 const CartItem = ({ onContinueShopping }) => {
